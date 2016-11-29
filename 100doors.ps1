3e5a414e84228e961cbfbc0635c77e6f4cd016a1
johngruss4 <jgruss@gwu.edu>
2016-11-29 16:10:11 -0500

    functioning solution
    adjusted formatting

--- 100doors.ps1
+++ 100doors.ps1
@@ -2,18 +2,16 @@
 function doors($q){
 $doors = foreach($number in 1..$q){New-Object psobject -property @{Name="Door $number";Open=$false}}
 $i=1
-
 do{
-
     foreach($number in 1..$q){if($number%$i -eq 0){toggle ($number-1)}}
     $i++
-    }until($i -gt $q)
+}until($i -gt $q)
     $doors
-    }
+}
 function Toggle($a){
     If($doors[$a].Open -eq $false){$doors[$a].Open=$true}
     Elseif($doors[$a].Open -eq $true){$doors[$a].Open=$false}
-    }
+}
 
 
 
